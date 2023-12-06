f483184b4bc66d30a0013f7e161ecfc8b8c2c32a
Alex May <psychicteeth@gmail.com>
2023-12-06 22:01:28 +0000

    Taking it right back to the base hello world example

--- src/main.c
+++ src/main.c
@@ -11,8 +11,32 @@
 
 #include "pd_api.h"
 
-static PlaydateAPI* pd = NULL;
-static LCDFont* font;
+static int update(void* userdata);
+const char* fontpath = "/System/Fonts/Asheville-Sans-14-Bold.pft";
+LCDFont* font = NULL;
+
+#ifdef _WINDLL
+__declspec(dllexport)
+#endif
+int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t arg)
+{
+	(void)arg; // arg is currently only used for event = kEventKeyPressed
+
+	if ( event == kEventInit )
+	{
+		const char* err;
+		font = pd->graphics->loadFont(fontpath, &err);
+		
+		if ( font == NULL )
+			pd->system->error("%s:%i Couldn't load font %s: %s", __FILE__, __LINE__, fontpath, err);
+
+		// Note: If you set an update callback in the kEventInit handler, the system assumes the game is pure C and doesn't run any Lua code in the game
+		pd->system->setUpdateCallback(update, pd);
+	}
+	
+	return 0;
+}
+
 
 #define TEXT_WIDTH 86
 #define TEXT_HEIGHT 16
@@ -22,14 +46,16 @@ int y = (240-TEXT_HEIGHT)/2;
 int dx = 1;
 int dy = 2;
 
-static int
-update(__attribute__ ((unused)) void* ud)
+static int update(void* userdata)
 {
+	PlaydateAPI* pd = userdata;
+	
 	pd->graphics->clear(kColorWhite);
-	//pd->graphics->drawText(font, NULL, NULL, "Hello World!", strlen("Hello World!"), kASCIIEncoding, x, y, kDrawModeCopy, 0, LCDMakeRect(0,0,0,0));
+	pd->graphics->setFont(font);
 	pd->graphics->drawText("Hello World!", strlen("Hello World!"), kASCIIEncoding, x, y);
 
-	x += dx; y += dy;
+	x += dx;
+	y += dy;
 	
 	if ( x < 0 || x > LCD_COLUMNS - TEXT_WIDTH )
 		dx = -dx;
@@ -42,18 +68,3 @@ update(__attribute__ ((unused)) void* ud)
 	return 1;
 }
 
-
-int
-eventHandler(PlaydateAPI* playdate, PDSystemEvent event, __attribute__ ((unused)) uint32_t arg)
-{
-	if ( event == kEventInit )
-	{
-		pd = playdate;
-		pd->display->setRefreshRate(20);
-		pd->system->setUpdateCallback(update, NULL);
-
-		font = pd->graphics->loadFont("/System/Fonts/Asheville-Sans-14-Bold.pft", NULL);
-	}
-	
-	return 0;
-}
